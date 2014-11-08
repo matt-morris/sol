10687d58706fc1c120bad99d177ae4300d612dc2
Matt Morris <matt.morris@placewise.com>
2014-11-07 19:16:12 -0700

    whitespace

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
 ((function() {
 
   var suits = [
-    { name: 'spades', icon: '♠', color: 'black' },
-    { name: 'clubs', icon: '♣', color: 'black' },
-    { name: 'hearts', icon: '♥', color: 'red' },
-    { name: 'diamonds', icon: '♦', color: 'red' }
+    {
+      name: 'spades',
+      icon: '♠',
+      color: 'black'
+    },
+    {
+      name: 'clubs',
+      icon: '♣',
+      color: 'black'
+    },
+    {
+      name: 'hearts',
+      icon: '♥',
+      color: 'red'
+    },
+    {
+      name: 'diamonds',
+      icon: '♦',
+      color: 'red'
+    }
   ];
 
   var ranks = [
-    { name: 'ace', symbol: 'A' },
-    { name: 'deuce', symbol: '2' },
-    { name: 'trey', symbol: '3' },
-    { name: 'four', symbol: '4' },
-    { name: 'five', symbol: '5' },
-    { name: 'six', symbol: '6' },
-    { name: 'seven', symbol: '7' },
-    { name: 'eight', symbol: '8' },
-    { name: 'nine', symbol: '9' },
-    { name: 'ten', symbol: '10' },
-    { name: 'jack', symbol: 'J' },
-    { name: 'queen', symbol: 'Q' },
-    { name: 'king', symbol: 'K' }
+    { name: 'ace',    symbol: 'A' },
+    { name: 'deuce',  symbol: '2' },
+    { name: 'trey',   symbol: '3' },
+    { name: 'four',   symbol: '4' },
+    { name: 'five',   symbol: '5' },
+    { name: 'six',    symbol: '6' },
+    { name: 'seven',  symbol: '7' },
+    { name: 'eight',  symbol: '8' },
+    { name: 'nine',   symbol: '9' },
+    { name: 'ten',    symbol: '10' },
+    { name: 'jack',   symbol: 'J' },
+    { name: 'queen',  symbol: 'Q' },
+    { name: 'king',   symbol: 'K' }
   ];
 
   var buildDeck = function() {
@@ -60,27 +76,41 @@
       diamonds: []
     },
 
-    tableau: [
-      new Array(1),
-      new Array(2),
-      new Array(3),
-      new Array(4),
-      new Array(5),
-      new Array(6),
-      new Array(7)
-    ],
+    tableau: {
+      column1: new Array(1),
+      column2: new Array(2),
+      column3: new Array(3),
+      column4: new Array(4),
+      column5: new Array(5),
+      column6: new Array(6),
+      column7: new Array(7)
+    },
 
-    stock: [],
+    pile: {
+      stock: [],
+      waste: []
+    }
+  };
 
-    waste: []
+  var find = function(card) {
+    var value;
+    
+    // console.log('find('+card+')')
+    // for (var column in table.tableau) {
+    //   column.map(compare(column));
+    // }
+    // for (var pile in table.foundation) {
+    //   pile.map(compare(column));
+    // }
+    return value;
   };
 
   var deal = function() {
     var deck = shuffle(buildDeck());
 
     for (var i = 0;i < 7;i++) {
-      for (var j = i;j < 7;j++) {
-        table.tableau[j][i] = deck.pop();
+      for (var j = i + 1;j <= 7;j++) {
+        table.tableau['column' + j][i] = deck.pop();
       }
     }
 
@@ -104,6 +134,15 @@
 
       for (var i = 0;i < n;i++) {
         table.waste.push(table.stock.pop());
+      }
+    },
+
+    cursor: {
+      card: null,
+      select: function(card) {
+        if (card) return card;
+        
+        return find(card);
       }
     }
   };
